1dc07bfa9572f6d744f156c56e5c0b651c990720
Joao Pereira <jao.pereira@campus.fct.unl.pt>
2020-12-13 21:51:26 +0000

    code for the nodes is OK; just need to take care of some TODOs

--- src/betterNode.erl
+++ src/betterNode.erl
@@ -12,70 +12,88 @@ start_link() ->
 init([]) ->
     {ok, build, #state{phase = 0, id = self(), max = self()}}.
 
+%% Wait for the supervisor to signal that the ring is built before
+%% starting the algorithm execution.
 build({pointer, Pointer}, S = #state{}) ->
     io:format("Building ~p with pointer ~p~n", [self(), Pointer]),
     receive
-        start ->
-            gen_fsm:send_event(Pointer, {1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
-            {next_state, active, S#state{pointer = Pointer}}
+        start -> b1_initiate_msg(Pointer, S)
     end.
 
-active({1, Max, _Phase, _Counter}, S = #state{}) ->
-    testMaximum(Max, S#state.id),
+%% send initial msg <1, max, phase, 2^phase> to the neighbor
+%% TODO: think of a better name for this function
+b1_initiate_msg(Pointer, S = #state{}) ->
+    gen_fsm:send_event(Pointer, {m1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
+    {next_state, active, S#state{pointer = Pointer}}.
+
+
+%% an active process never receives a type m2 message
+active({m1, Max, _Phase, _Counter}, S = #state{}) ->
+    test_maximum(Max, S#state.id),
     if Max > S#state.max ->
         {next_state, waiting, S#state{max = Max}};
-        Max =< S#state.max ->
-            gen_fsm:send_event(S#state.pointer, {2, S#state.max}), %comentar esta linha para testar
-            {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
+    Max =< S#state.max ->
+        gen_fsm:send_event(S#state.pointer, {m2, S#state.max}), %comentar esta linha para testar
+        {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
     end.
+%%    case Max > S#state.max of
+%%        true ->
+%%            {next_state, waiting, S#state{max = Max}};
+%%        false ->
+%%            gen_fsm:send_event(S#state.pointer, {m2, S#state.max}), %comentar esta linha para testar
+%%            {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
+%%    end,
+
+
 
 potato({potato}, S = #state{}) ->
     {next_state, active, S#state{}}.
 
-passive({1, Max, Phase, Counter}, S = #state{}) ->
-    testMaximum(Max, S#state.id),
+passive({m1, Max, Phase, Counter}, S = #state{}) ->
+    test_maximum(Max, S#state.id),
     if Max >= S#state.max andalso Counter >= 1 ->
         if Counter > 1 ->
-            gen_fsm:send_event(S#state.pointer, {1, Max, Phase, Counter - 1}),
+            gen_fsm:send_event(S#state.pointer, {m1, Max, Phase, Counter - 1}),
             {next_state, passive, S#state{max = Max}}; %not sure about this state or terminates
-            Counter == 1 ->
-                gen_fsm:send_event(S#state.pointer, {1, Max, Phase, 0}),
-                {next_state, waiting, S#state{max = Max, phase = Phase}}
+        Counter == 1 ->
+            gen_fsm:send_event(S#state.pointer, {m1, Max, Phase, 0}),
+            {next_state, waiting, S#state{max = Max, phase = Phase}}
         end;
-        Max < S#state.max orelse Counter < 1 ->
-            gen_fsm:send_event(S#state.pointer, {1, Max, Phase, 0}),
-            {next_state, passive, S#state{}} %not sure about this state or terminates
+    Max < S#state.max orelse Counter < 1 ->
+        gen_fsm:send_event(S#state.pointer, {m1, Max, Phase, 0}),
+        {next_state, passive, S#state{}} %not sure about this state or terminates
     end;
-passive({2, Max}, S = #state{}) ->
-    testMaximum(Max, S#state.id),
+passive({m2, Max}, S = #state{}) ->
+    test_maximum(Max, S#state.id),
     if Max < S#state.max ->
+        %% skip this msg
         {next_state, passive, S#state{}};
-        Max >= S#state.max ->
-            gen_fsm:send_event(S#state.pointer, {2, Max}),
-            {next_state, passive, S#state{}}
+    Max >= S#state.max ->
+        gen_fsm:send_event(S#state.pointer, {m2, Max}),
+        {next_state, passive, S#state{}}
     end.
 
-waiting({2, Max}, S = #state{}) ->
-    testMaximum(Max, S#state.id),
-    if S#state.max == Max ->
-        gen_fsm:send_event(S#state.pointer, {1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
-        {next_state, active, S#state{phase = S#state.phase + 1}};
-        S#state.max /= max ->
-            {stop, error, S#state{}}
-    end;
-waiting({1, Max, _Phase, _Counter}, S = #state{}) ->
-    testMaximum(Max, S#state.id),
-    {next_state, passive, S#state{}}.
+
+waiting({m1, Max, _Phase, _Counter}, S = #state{}) ->
+    test_maximum(Max, S#state.id),
+    {next_state, passive, S#state{}};
+waiting({m2, Max}, S = #state{max = Max}) ->
+    %% if a message of type M2 arrives then Max should be equal to S#state.max
+    test_maximum(Max, S#state.id),
+    b1_initiate_msg(S#state.pointer, S#state{phase = S#state.phase + 1});
+waiting({m2, _Max}, S = #state{}) ->
+    % if Max =/= S#state.max then some error has occurred
+    {stop, error, S#state{}}.
 
 handle_event(restart, _StateName, _State) ->
     io:format("Restarting waiting for builds ~p~n", [self()]),
     {next_state, build, #state{phase = 0, id = self(), max = self()}}.
 
-testMaximum(Max, Id) ->
-    if Max == Id ->
-        io:format("Maximum is: ~p~n", [Id]),
-        %{normal, active, S#state{}},
-        exit(shutdown);
-        Max /= Id ->
-            false
-    end.
+test_maximum(Max, Max) ->
+    %% if a node received his Id as his predecessor's Max,
+    %% then his Id is the global maximum - can terminate the algorithm
+    io:format("Maximum is: ~p~n", [Max]),
+    %% TODO(?): terminate processes in an orderly fashion - maybe it can just be the order of the list in the supervisor
+    exit(shutdown);
+test_maximum(_, _) ->
+    false.
