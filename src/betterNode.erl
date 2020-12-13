773889ce113d23c4cf1d0753d6a75dceaae318df
Joao Pereira <jao.pereira@campus.fct.unl.pt>
2020-12-13 20:08:01 +0000

    formatting

--- src/betterNode.erl
+++ src/betterNode.erl
@@ -7,75 +7,75 @@
 -record(state, {phase, id, max, pointer}).
 
 start_link() ->
-	gen_fsm:start_link(?MODULE, [], []).
+    gen_fsm:start_link(?MODULE, [], []).
 
 init([]) ->
-	{ok, build, #state{phase = 0, id=self(), max=self()}}.
+    {ok, build, #state{phase = 0, id = self(), max = self()}}.
 
-build({pointer, Pointer}, S=#state{}) ->
-	io:format("Building ~p with pointer ~p~n", [self(), Pointer]),
-	receive 
-		start ->
-			gen_fsm:send_event(Pointer, {1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
-			{next_state, active, S#state{pointer=Pointer}}
-	end.
+build({pointer, Pointer}, S = #state{}) ->
+    io:format("Building ~p with pointer ~p~n", [self(), Pointer]),
+    receive
+        start ->
+            gen_fsm:send_event(Pointer, {1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
+            {next_state, active, S#state{pointer = Pointer}}
+    end.
 
-active({1, Max, _Phase, _Counter}, S=#state{}) ->
-	testMaximum(Max, S#state.id),
-	if Max > S#state.max ->
-		{next_state, waiting, S#state{max = Max}};
-	Max =< S#state.max ->
-		gen_fsm:send_event(S#state.pointer, {2, S#state.max}), %comentar esta linha para testar
-		{next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
-	end.
+active({1, Max, _Phase, _Counter}, S = #state{}) ->
+    testMaximum(Max, S#state.id),
+    if Max > S#state.max ->
+        {next_state, waiting, S#state{max = Max}};
+        Max =< S#state.max ->
+            gen_fsm:send_event(S#state.pointer, {2, S#state.max}), %comentar esta linha para testar
+            {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
+    end.
 
-potato({potato}, S=#state{}) ->
-	{next_state, active, S#state{}}.
+potato({potato}, S = #state{}) ->
+    {next_state, active, S#state{}}.
 
-passive({1, Max, Phase, Counter}, S=#state{}) ->
-	testMaximum(Max, S#state.id),
-	if Max >= S#state.max andalso Counter >= 1 ->
-		if Counter > 1 ->
-			gen_fsm:send_event(S#state.pointer, {1, Max, Phase, Counter-1}),
-			{next_state, passive, S#state{max = Max}}; %not sure about this state or terminates
-		Counter == 1 ->
-			gen_fsm:send_event(S#state.pointer, {1, Max, Phase, 0}),
-			{next_state, waiting, S#state{max = Max, phase = Phase}}
-		end;
-	Max < S#state.max orelse Counter < 1 ->
-		gen_fsm:send_event(S#state.pointer, {1, Max, Phase, 0}),
-		{next_state, passive, S#state{}} %not sure about this state or terminates
-	end;
-passive({2, Max}, S=#state{}) ->
-	testMaximum(Max, S#state.id),
-	if Max < S#state.max ->
-		{next_state, passive, S#state{}};
-	Max >= S#state.max ->
-		gen_fsm:send_event(S#state.pointer, {2, Max}),
-		{next_state, passive, S#state{}}
-	end.
+passive({1, Max, Phase, Counter}, S = #state{}) ->
+    testMaximum(Max, S#state.id),
+    if Max >= S#state.max andalso Counter >= 1 ->
+        if Counter > 1 ->
+            gen_fsm:send_event(S#state.pointer, {1, Max, Phase, Counter - 1}),
+            {next_state, passive, S#state{max = Max}}; %not sure about this state or terminates
+            Counter == 1 ->
+                gen_fsm:send_event(S#state.pointer, {1, Max, Phase, 0}),
+                {next_state, waiting, S#state{max = Max, phase = Phase}}
+        end;
+        Max < S#state.max orelse Counter < 1 ->
+            gen_fsm:send_event(S#state.pointer, {1, Max, Phase, 0}),
+            {next_state, passive, S#state{}} %not sure about this state or terminates
+    end;
+passive({2, Max}, S = #state{}) ->
+    testMaximum(Max, S#state.id),
+    if Max < S#state.max ->
+        {next_state, passive, S#state{}};
+        Max >= S#state.max ->
+            gen_fsm:send_event(S#state.pointer, {2, Max}),
+            {next_state, passive, S#state{}}
+    end.
 
-waiting({2, Max}, S=#state{}) ->
-	testMaximum(Max, S#state.id),
-	if S#state.max == Max ->
-		gen_fsm:send_event(S#state.pointer, {1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
-		{next_state, active, S#state{phase= S#state.phase + 1}};
-	S#state.max /= max ->
-		{stop, error, S#state{}}
-	end;
-waiting({1, Max, _Phase, _Counter}, S=#state{}) ->
-	testMaximum(Max, S#state.id),
-	{next_state, passive, S#state{}}.
+waiting({2, Max}, S = #state{}) ->
+    testMaximum(Max, S#state.id),
+    if S#state.max == Max ->
+        gen_fsm:send_event(S#state.pointer, {1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
+        {next_state, active, S#state{phase = S#state.phase + 1}};
+        S#state.max /= max ->
+            {stop, error, S#state{}}
+    end;
+waiting({1, Max, _Phase, _Counter}, S = #state{}) ->
+    testMaximum(Max, S#state.id),
+    {next_state, passive, S#state{}}.
 
 handle_event(restart, _StateName, _State) ->
-	io:format("Restarting waiting for builds ~p~n", [self()]),
-	{next_state, build, #state{phase = 0, id=self(), max=self()}}.
+    io:format("Restarting waiting for builds ~p~n", [self()]),
+    {next_state, build, #state{phase = 0, id = self(), max = self()}}.
 
 testMaximum(Max, Id) ->
-	if Max == Id ->
-		io:format("Maximum is: ~p~n", [Id]),
-		%{normal, active, S#state{}},
-		exit(shutdown);
-	Max /= Id ->
-		false
-	end.
+    if Max == Id ->
+        io:format("Maximum is: ~p~n", [Id]),
+        %{normal, active, S#state{}},
+        exit(shutdown);
+        Max /= Id ->
+            false
+    end.
--- src/supv.erl
+++ src/supv.erl
@@ -1,36 +1,36 @@
 -module(supv).
 -compile(export_all).
 
-start(M,NumNodes) ->
+start(M, NumNodes) ->
     register(?MODULE, Pid = spawn_link(?MODULE, init, [{M, NumNodes}])),
     Pid.
 
-init({M,NumNodes}) ->
+init({M, NumNodes}) ->
     process_flag(trap_exit, true),
     startNodes(M, NumNodes, []).
 
-startNodes(M, 1, [H|T]) ->
-	{_,Pid} = apply(M, start_link, []),
-	io:format("Started~p~n", [Pid]),
-	List = [H|T] ++ [Pid],
-	buildNodes(List, H),
-	startProtocol(List),
-	receiving(List, M);
+startNodes(M, 1, [H | T]) ->
+    {_, Pid} = apply(M, start_link, []),
+    io:format("Started~p~n", [Pid]),
+    List = [H | T] ++ [Pid],
+    buildNodes(List, H),
+    startProtocol(List),
+    receiving(List, M);
 startNodes(M, NumNodes, List) ->
-	{_,Pid} = apply(M, start_link, []),
-	io:format("Started~p~n", [Pid]),
-	startNodes(M, NumNodes-1, List ++ [Pid]).
+    {_, Pid} = apply(M, start_link, []),
+    io:format("Started~p~n", [Pid]),
+    startNodes(M, NumNodes - 1, List ++ [Pid]).
 
-buildNodes([H], First) -> 
-	gen_fsm:send_event(H ,{pointer, First});
-buildNodes([H1, H2|T], First) ->
-	gen_fsm:send_event(H1 ,{pointer, H2}),
-	buildNodes([H2|T], First).
+buildNodes([H], First) ->
+    gen_fsm:send_event(H, {pointer, First});
+buildNodes([H1, H2 | T], First) ->
+    gen_fsm:send_event(H1, {pointer, H2}),
+    buildNodes([H2 | T], First).
 
 startProtocol([]) -> ok;
-startProtocol([H|T]) -> 
-	H ! start,
-	startProtocol(T).
+startProtocol([H | T]) ->
+    H ! start,
+    startProtocol(T).
 
 receiving(List, M) ->
     receive
@@ -40,16 +40,16 @@ receiving(List, M) ->
             io:format("Process ~p exited for reason ~p~n", [From, Reason]),
             RList = lists:delete(From, List),
             sendStopAll(RList),
-            {_,Pid} = apply(M, start_link, []),
+            {_, Pid} = apply(M, start_link, []),
             io:format("Started~p~n", [Pid]),
-            [H|T] = RList ++ [Pid],
-            buildNodes([H|T], H),
-            receiving([H|T], M)
+            [H | T] = RList ++ [Pid],
+            buildNodes([H | T], H),
+            receiving([H | T], M)
     end.
 
 sendStopAll([]) -> ok;
 sendStopAll([H]) -> gen_fsm:send_all_state_event(H, restart);
-sendStopAll([H|T]) -> 
-	gen_fsm:send_all_state_event(H, restart),
-	sendStopAll(T).
+sendStopAll([H | T]) ->
+    gen_fsm:send_all_state_event(H, restart),
+    sendStopAll(T).
 
