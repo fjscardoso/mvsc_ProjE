a9854f128be01eb7ec7497dcc2f7ec0dcf716b66
Joao Pereira <jao.pereira@campus.fct.unl.pt>
2020-12-14 17:15:47 +0000

    preparing Node to received its Id (different from pid) in its init function

--- src/betterNode.erl
+++ src/betterNode.erl
@@ -6,48 +6,45 @@
 
 -record(state, {phase, id, max, pointer}).
 
-start_link() ->
-    gen_fsm:start_link(?MODULE, [], []).
+start_link(Id) ->
+    gen_fsm:start_link(?MODULE, [Id], []).
 
-init([]) ->
-    {ok, build, #state{phase = 0, id = self(), max = self()}}.
+init([Id]) ->
+    {ok, build, #state{phase = 0, id = Id, max = Id}}.
 
 %% Wait for the supervisor to signal that the ring is built before
 %% starting the algorithm execution.
 build({pointer, Pointer}, S = #state{}) ->
     io:format("Building ~p with pointer ~p~n", [self(), Pointer]),
     receive
-        start -> b1_initiate_msg(Pointer, S)
+        start -> b1_initiate_msg(S#state{pointer = Pointer})
     end.
+
 
 %% send initial msg <1, max, phase, 2^phase> to the neighbor
 %% TODO: think of a better name for this function
-b1_initiate_msg(Pointer, S = #state{}) ->
-    gen_fsm:send_event(Pointer, {m1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
-    {next_state, active, S#state{pointer = Pointer}}.
+b1_initiate_msg(S = #state{}) ->
+    gen_fsm:send_event(S#state.pointer, {m1, S#state.max, S#state.phase, math:pow(2, S#state.phase)}),
+    {next_state, active, S}.
 
 
-%% an active process never receives a type m2 message
 active({m1, Max, _Phase, _Counter}, S = #state{}) ->
     test_maximum(Max, S#state.id),
-    if Max > S#state.max ->
-        {next_state, waiting, S#state{max = Max}};
-    Max =< S#state.max ->
-        gen_fsm:send_event(S#state.pointer, {m2, S#state.max}), %comentar esta linha para testar
-        {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
-    end.
-%%    case Max > S#state.max of
-%%        true ->
-%%            {next_state, waiting, S#state{max = Max}};
-%%        false ->
-%%            gen_fsm:send_event(S#state.pointer, {m2, S#state.max}), %comentar esta linha para testar
-%%            {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
-%%    end,
+    case Max > S#state.max of
+        true ->
+            {next_state, waiting, S#state{max = Max}};
+        false ->
+            gen_fsm:send_event(S#state.pointer, {m2, S#state.max}), %comentar esta linha para testar
+            {next_state, passive, S#state{}}  %trocar de passive para potato para testar, inicial {next_state, passive, S#state{}}
+    end;
+active({m2, _}, S = #state{}) ->
+    %% an active process should never receive a type m2 message
+    {stop, error, S#state{}}.
 
 
 
-potato({potato}, S = #state{}) ->
-    {next_state, active, S#state{}}.
+%%potato({potato}, S = #state{}) ->
+%%    {next_state, active, S#state{}}.
 
 passive({m1, Max, Phase, Counter}, S = #state{}) ->
     test_maximum(Max, S#state.id),
@@ -66,7 +63,7 @@ passive({m1, Max, Phase, Counter}, S = #state{}) ->
 passive({m2, Max}, S = #state{}) ->
     test_maximum(Max, S#state.id),
     if Max < S#state.max ->
-        %% skip this msg
+        %% just skip this msg
         {next_state, passive, S#state{}};
     Max >= S#state.max ->
         gen_fsm:send_event(S#state.pointer, {m2, Max}),
@@ -80,14 +77,15 @@ waiting({m1, Max, _Phase, _Counter}, S = #state{}) ->
 waiting({m2, Max}, S = #state{max = Max}) ->
     %% if a message of type M2 arrives then Max should be equal to S#state.max
     test_maximum(Max, S#state.id),
-    b1_initiate_msg(S#state.pointer, S#state{phase = S#state.phase + 1});
+    b1_initiate_msg(S#state{phase = S#state.phase + 1});
 waiting({m2, _Max}, S = #state{}) ->
     % if Max =/= S#state.max then some error has occurred
     {stop, error, S#state{}}.
 
-handle_event(restart, _StateName, _State) ->
+handle_event(restart, _StateName, S = #state{id = Id}) ->
     io:format("Restarting waiting for builds ~p~n", [self()]),
-    {next_state, build, #state{phase = 0, id = self(), max = self()}}.
+    %% restart the algorithm
+    {next_state, build, S#state{phase = 0, max = Id}}.
 
 test_maximum(Max, Max) ->
     %% if a node received his Id as his predecessor's Max,
